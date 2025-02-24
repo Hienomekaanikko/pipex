f0e59bfeef7ec0c9db37e194dcf82ea4c231a07b
Mikko Suokas <msuokas@c1r3p5.hive.fi>
2025-02-24 15:27:49 +0200

    update

--- error.c
+++
@@ -1,7 +0,0 @@
-#include "pipex.h"
-
-void	msg_error(char *msg)
-{
-	perror(msg);
-	exit (1);
-}
--- libft/ft_error.c
+++
@@ -1,7 +0,0 @@
-#include "libft.h"
-
-int	ft_error(char *msg)
-{
-	ft_putendl_fd(msg, 2);
-	return (1);
-}
