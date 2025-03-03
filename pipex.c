5fb51b989fcfdba57d803edc1e953d9386a1ae52
mikkosuokas <mikesuokas@gmail.com>
2025-03-03 11:16:29 +0200

    update

--- pipex.c
+++ pipex.c
@@ -12,28 +12,8 @@
 
 #include "pipex.h"
 
-void	init_data(t_data *data)
-{
-	data->paths = NULL;
-	data->path = NULL;
-	data->cmd1 = NULL;
-	data->cmd2 = NULL;
-	data->in = -1;
-	data->out = -1;
-	data->pipe[0] = -1;
-	data->pipe[1] = -1;
-}
-
 void	ft_exit(t_data *data, char *msg, int exitcode)
 {
-	if (data->paths)
-		ft_free_split(data->paths);
-	if (data->path)
-		free(data->path);
-	if (data->cmd1)
-		ft_free_split(data->cmd1);
-	if (data->cmd2)
-		ft_free_split(data->cmd2);
 	if (data->in != -1)
 		close(data->in);
 	if (data->out != -1)
@@ -42,28 +22,60 @@ void	ft_exit(t_data *data, char *msg, int exitcode)
 		close(data->pipe[0]);
 	if (data->pipe[1] != -1)
 		close(data->pipe[1]);
-	if (msg != NULL)
+	if (data->path)
+		free(data->path);
+	if (data->paths)
+		ft_free_split(data->paths);
+	if (data->cmd1)
+		ft_free_split(data->cmd1);
+	if (data->cmd2)
+		ft_free_split(data->cmd2);
+	if (msg)
 		perror(msg);
 	exit(exitcode);
 }
 
-void	get_path(t_data *data, char *command, char **envp)
+void	prep_env(int argc, char **argv, char **envp, t_data *data)
 {
-	int		i;
+	data->in = open(argv[1], O_RDONLY);
+	data->out = open(argv[3], O_TRUNC, O_CREAT, O_RDWR, 0644);
+	pipe(data->pipe);
+}
+
+void	init_data(t_data *data)
+{
+	data->path = NULL;
+	data->paths = NULL;
+	data->cmd1 = NULL;
+	data->cmd2 = NULL;
+	data->in = -1;
+	data->out = -1;
+	data->pipe[0] = -1;
+	data->pipe[1] = -1;
+}
+
+void	find_path(t_data *data, char *cmd, char **envp)
+{
 	char	*path;
+	int		i;
 
 	i = 0;
-	while (envp[i] && ft_strncmp(envp[i], "PATH=", 5) != 0)
+	while (envp[i] && ft_strcmp(envp[i], "=PATH", 5) != 0)
 		i++;
 	if (!envp[i])
-		ft_exit(data, "path not found", 127);
+		ft_exit(data, "ERROR: Path not found\n", 127);
 	data->paths = ft_split(envp[i] + 5, ':');
+	if (!data->paths)
+		ft_exit(data, "ERROR: Memory allocation\n", 1);
 	i = 0;
-	while (data->paths[i])
+	while (envp[i])
 	{
 		path = ft_strjoin(data->paths[i], "/");
-		data->path = ft_strjoin(path, command);
-		free(path);
+		if (!path)
+			ft_exit(data, "ERROR: Memory allocation\n", 1);
+		data->path = ft_strjoin(path, cmd);
+		if (!data->path)
+			ft_exit(data, "ERROR: Memory allocation\n", 1);
 		if (access(data->path, F_OK | X_OK) == 0)
 		{
 			ft_free_split(data->paths);
@@ -73,98 +85,56 @@ void	get_path(t_data *data, char *command, char **envp)
 		data->path = NULL;
 		i++;
 	}
-	ft_free_split(data->paths);
-	data->paths = NULL;
-	ft_exit(data, "command not found", 127);
+	ft_exit(data, "ERROR: Path not found\n", 127);
 }
 
-void	prep_env(t_data *data, int argc, char **argv)
+void	child_one(t_data *data, char cmd, char **envp)
 {
-	data->in = open(argv[1], O_RDONLY);
-	if (data->in < 0)
-		perror("Input file error");
-	if ((data->out = open(argv[argc - 1], O_TRUNC | O_CREAT | O_RDWR, 0644)) < 0)
-	{
-		if (data->in >= 0)
-			close(data->in);
-		ft_exit(data, "output fail", 1);
-	}
-	if (pipe(data->pipe) < 0)
-	{
-		if (data->in >= 0)
-			close(data->in);
-		close(data->out);
-		ft_exit(data, "pipe failed", 1);
-	}
-}
-
-void	child_one(t_data *data, char *command, char **envp)
-{
-	if (!command[0])
-		ft_exit(data, "Empty command", 1);
-	data->cmd1 = ft_split(command, ' ');
+	find_path(data, cmd, envp);
+	data->cmd1 = ft_split(cmd, ' ');
 	if (!data->cmd1)
-		ft_exit(data, "Memory allocation failed", 1);
-	get_path(data, data->cmd1[0], envp);
-	if (data->in >= 0)
-		dup2(data->in, 0);
+		ft_exit(data, "ERROR: Memory allocation\n", 1);
+	dup2(data->in, 0);
+	close(data->in);
 	dup2(data->pipe[1], 1);
-	close(data->pipe[0]);
 	close(data->pipe[1]);
-	if (data->in >= 0)
-		close(data->in);
-	close(data->out);
-	if (data->in < 0 || execve(data->path, data->cmd1, envp) == -1)
-		ft_exit(data, "Execve failed in child one", 1);
+	execve(data->path, data->cmd1, envp);
 }
 
-void	child_two(t_data *data, char *command, char **envp)
+void	child_two(t_data *data, char cmd, char **envp)
 {
-	if (!command[0])
-		ft_exit(data, "Empty command", 1);
-	data->cmd2 = ft_split(command, ' ');
+	find_path(data, cmd, envp);
+	data->cmd2 = ft_split(cmd, ' ');
 	if (!data->cmd2)
-		ft_exit(data, "Memory allocation failed", 1);
-	get_path(data, data->cmd2[0], envp);
-	dup2(data->pipe[0], 0);
+		ft_exit(data, "ERROR: Memory allocation\n", 1);
 	dup2(data->out, 1);
-	close(data->pipe[0]);
-	close(data->pipe[1]);
-	close(data->in);
 	close(data->out);
-	if (execve(data->path, data->cmd2, envp) == -1)
-		ft_exit(data, "Execve failed in child two", 1);
+	dup2(data->pipe[0], 0);
+	close(data->pipe[0]);
+	execve(data->path, data->cmd2, envp);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
 	t_data	data;
-	int		status1;
-	int		status2;
 
-	if (argc != 5)
+	if (argc == 5)
 	{
-		ft_putendl_fd("Usage: ./pipex infile cmd1 cmd2 outfile", 2);
-		exit(1);
+		init_data(&data);
+		prep_env(argc, argv, envp, &data);
+		data.pid1 = fork();
+		if (data.pid1 == -1)
+			ft_exit(&data, "Error while forking\n", 1);
+		else if (data.pid2 == 0)
+			child_one(&data, argv[2], envp);
+		data.pid2 = fork();
+		if (data.pid2 == -1)
+			ft_exit(&data, "Error while forking\n", 1);
+		else if (data.pid2 == 0)
+			child_two(&data, argv[3], envp);
+		init_children(&data, argv, envp);
+		waitpid(data.pid1, NULL, 0);
+		waitpid(data.pid2, NULL, 0);
 	}
-	init_data(&data);
-	prep_env(&data, argc, argv);
-	data.pid1 = fork();
-	if (data.pid1 == 0)
-		child_one(&data, argv[2], envp);
-	data.pid2 = fork();
-	if (data.pid2 == 0)
-		child_two(&data, argv[3], envp);
-	close(data.pipe[0]);
-	close(data.pipe[1]);
-	if (data.in >= 0)
-		close(data.in);
-	close(data.out);
-	waitpid(data.pid1, &status1, 0);
-	waitpid(data.pid2, &status2, 0);
-	if (WIFEXITED(status2))
-		return (WEXITSTATUS(status2));
-	else if (WIFEXITED(status1))
-		return (WEXITSTATUS(status1));
-	return (1);
+	return (0);
 }
